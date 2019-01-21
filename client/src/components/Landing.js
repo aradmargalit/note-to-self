99541ecc4b8d334f26b25b4d7822353972d71ab8
Arad Margalit <aradmar@gmail.com>
2019-01-21 13:32:00 -0800

    Login routing fixed

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
-const Landing = () => {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>Note to Self</h1>
-      You know what to do. Write it!
-    </div>
-  );
-};
+class Landing extends Component {
+  render() {
+    return this.props.auth ? (
+      <Redirect
+        to={{
+          pathname: '/dashboard',
+          state: { from: this.props.location },
+        }}
+      />
+    ) : (
+      <div style={{ textAlign: 'center' }}>
+        <h1>Note to Self</h1>
+        You know what to do. Write it!
+      </div>
+    );
+  }
+}
 
-export default Landing;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Landing);
