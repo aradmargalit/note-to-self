773c9a4dd903fdb5057b7932200a1a2dcfc7ec18
Arad Margalit <aradmar@gmail.com>
2019-01-26 11:27:18 -0800

    Don't allow dashboard if not logged in

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -4,6 +4,7 @@ import MemoryList from './MemoryList';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 import { Container } from 'react-bootstrap';
+import { Redirect } from 'react-router-dom';
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -11,17 +12,28 @@ class Dashboard extends Component {
   }
 
   render() {
-    return (
+    return this.props.auth != false ? (
       <Container style={{ padding: '30px 20px' }}>
         <AddMemory />
         <h4 style={{ paddingTop: '30px' }}>Your Memories</h4>
         <MemoryList />
       </Container>
+    ) : (
+      <Redirect
+        to={{
+          pathname: '/',
+          state: { from: this.props.location },
+        }}
+      />
     );
   }
 }
 
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
 export default connect(
-  null,
+  mapStateToProps,
   actions
 )(Dashboard);
